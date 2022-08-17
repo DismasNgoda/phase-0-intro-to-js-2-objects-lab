e7719e02de06f4dd3eaba1b5860d6ca7f77fe05f
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-08-17 21:43:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = {name: "Sam", streetAddress: "Nairobi"};
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const employee1 = {...obj};
+    employee1[key] = '11 Broadway';
+    
+    return employee1;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    employee.streetAddress = '12 Broadway';
+    return employee;
+};
+
+function deleteFromEmployeeByKey(obj, key){
+    let newEmployee = {...obj}
+    delete newEmployee['name'];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+}
+
+
+
